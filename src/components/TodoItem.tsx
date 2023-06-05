4266b203bd49bf9779ae76bb3d78dfb5d784e0df
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-06-05 23:44:34 +0400

    add initial files

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+type TodoTypes = {
+    id: string
+    title: string
+    complete: boolean
+    toggleTodo: (id:string, complete: boolean) => void
+}
+
+export function TodoItem({id,title,complete, toggleTodo}:TodoTypes){
+    return(
+        <li className="flex gap-1 items-center">
+            <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={complete} onChange={e=>toggleTodo(id,e.target.checked)} />
+            <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+        </li>
+    )
+}
